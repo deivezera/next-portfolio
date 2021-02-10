a3aa925b9e8b8b74de6fbf6f90daa6698b5a200d
deivezera <dzabroski@gmail.com>
2021-02-10 10:01:51 -0300

    svg treatment

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from '../styles/pages/Home'
+import BackgroundSVG from '../assets/BackgroundSVG.svg'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <BackgroundSVG />
+      <h1>Em construção</h1>
+      <p>Portfolio de desenvolvimento de Davi Szeremeta Zabroski</p>
+    </Container>
+  )
+}
+export default Home
