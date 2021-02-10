74157dc5513ed9919d4d66524b68641cdf34803d
deivezera <dzabroski@gmail.com>
2021-02-10 10:18:06 -0300

    svg treat

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Container } from '../styles/pages/Home'
-import BackgroundSVG from '../assets/BackgroundSVG.svg'
+// import BackgroundSVG from '../assets/BackgroundSVG.svg'
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <BackgroundSVG />
+      {/* <BackgroundSVG /> */}
       <h1>Em construção</h1>
       <p>Portfolio de desenvolvimento de Davi Szeremeta Zabroski</p>
     </Container>
