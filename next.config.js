19f01912d695cf15503d718691117bcd796e8dc5
deivezera <dzabroski@gmail.com>
2021-02-10 23:07:26 -0300

    svg

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const withImages = require('next-images')
 module.exports = withImages({
+  esModule: true,
   exclude: path.resolve(__dirname, 'assets'),
   webpack(config, options) {
     return config
