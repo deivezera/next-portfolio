c5cee9c0f98b7e3d77d82d901e118d82b43777fa
deivezera <dzabroski@gmail.com>
2021-02-10 23:33:39 -0300

    deploy svg

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 
-// next.config.js
-const path = require('path');
 const withImages = require('next-images')
 module.exports = withImages({
   esModule: true,
-  exclude: path.resolve(__dirname, 'assets'),
-  webpack(config, options)  {
-    config.module.rules.push({
-        test: /\.svg$/,
-        use: [
-            {
-                loader: "@svgr/webpack",
-                options: {
-                    svgo: false, // Optimization caused bugs with some of my SVGs
-                },
-            },
-        ],
-    });
-}
 })
